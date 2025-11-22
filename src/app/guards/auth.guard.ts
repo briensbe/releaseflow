2daf3d0e5a572d620df3e92906ce8b2ef78fa27e
briensbe <briensbe@yahoo.com>
2025-11-22 08:04:00 +0100

    SIDE-33 rendre activable par environnement la protection email/password

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { SupabaseService } from '../../services/supabase.service';
+import { environment } from '../../environments/environment';
 
 export const AuthGuard: CanActivateFn = async (route, state) => {
+
+  // --- désactivé en dev ---
+  if (!environment.enableAuth) {
+    return true;
+  }
+
   const supabaseService = inject(SupabaseService);
   const router = inject(Router);
 
---
+++ src/environments/environment prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  enableAuth: true,
+  // le compte supabase de bolt
+  // supabaseUrl: 'https://ahfuwnnmukxervlxshtz.supabase.co',
+  // supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoZnV3bm5tdWt4ZXJ2bHhzaHR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxMDI0NDQsImV4cCI6MjA3NzY3ODQ0NH0.9A4Q0gmfpRL-DwyHajjB78D_lNEIKimMXL_LUy37kO8'
+  // mon compte supabase bbr - benjuju
+  supabaseUrl: 'https://zrkzkteruybaujoekdan.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpya3prdGVydXliYXVqb2VrZGFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxODAzNTUsImV4cCI6MjA3Nzc1NjM1NX0._UT310p01cnP0oH0OsEdaE06XuzhlzEC1UWJruld_N8',
+};
