c3df039c70aad199fa1682292f5e4c7cbc60c7ff
briensbe <briensbe@yahoo.com>
2025-11-27 21:48:25 +0100

    SIDE-35 petit texte au moment de la connexion

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -12,20 +12,26 @@ import { FormsModule } from '@angular/forms';
 export class LoginComponent {
   email = signal('');
   password = signal('');
+  loading = false;
 
   private readonly supabaseService = inject(SupabaseService);
   private readonly router = inject(Router);
 
   async signInWithEmail() {
-    const { error } = await this.supabaseService.signInWithEmail({
-      email: this.email(),
-      password: this.password(),
-    });
-    if (error) {
-      alert('Error signing in: ' + error.message);
-    } else {
-      // this.router.navigate(['/profile']); // ancien comportement par défaut isssu de supabase-auth
-      this.router.navigate(['/compact']);
+    this.loading = true;
+    try {
+      const { error } = await this.supabaseService.signInWithEmail({
+        email: this.email(),
+        password: this.password(),
+      });
+      if (error) {
+        alert('Error signing in: ' + error.message);
+      } else {
+        // this.router.navigate(['/profile']); // ancien comportement par défaut isssu de supabase-auth
+        this.router.navigate(['/compact']);
+      }
+    } finally {
+      this.loading = false;
     }
   }
 
